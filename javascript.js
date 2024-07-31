83e23bcba29d2493143ada34603f0ca95371c12c
Senorpedrito <pieterwattel@gmail.com>
2024-07-31 18:12:08 +0200

    make function getComputerChoice
    I made a function that gives a random choice when run.
    That means it will give back either "rock", "paper", or "scissors"
    this can be used for the turns of the computer

--- javascript.js
+++ javascript.js
@@ -1,8 +1,13 @@
-function getComputerChoice () {
-    let num = (Math.floor(Math.random)*3)
-    return num
+//getting the computer answer
+function getComputerChoice() {
+    //let the math.random operator make a number between 1 and 3.
+    let num = Math.floor(Math.random()*3)
+    //transform the number to a string answer
+    if (num == 0) {
+        return "rock"
+    } else if (num == 1) {
+        return "paper"
+    } else if (num == 2) {
+        return "scissors"
+    }
 }
-
-console.log(getComputerChoice ())
-
-console.log("5")
